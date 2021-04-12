8c413add7aace8f5bb618748d0aaefe8b7f01df0
Shivang <daveshivang15@gmail.com>
2021-04-12 15:43:34 -0500

    Add basic configuration..

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/api/public', to: 'public#public'
+  get '/api/private', to: 'private#private'
+  get '/api/private-scoped', to: 'private#private_scoped'
 end
